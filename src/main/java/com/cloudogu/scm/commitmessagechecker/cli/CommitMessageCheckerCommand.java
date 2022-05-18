70791c83600d7d73d4c215119308131b050ed55e
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-05-18 10:54:32 +0200

    Implement commit message check command

---
+++ src/main/java/com/cloudogu/scm/commitmessagechecker/cli/CommitMessageCheckerCommand.java
@@ -0,0 +1,103 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.cloudogu.scm.commitmessagechecker.cli;
+
+import com.cloudogu.scm.commitmessagechecker.AvailableValidators;
+import com.cloudogu.scm.commitmessagechecker.Context;
+import com.cloudogu.scm.commitmessagechecker.Validator;
+import com.cloudogu.scm.commitmessagechecker.config.Configuration;
+import com.cloudogu.scm.commitmessagechecker.config.ConfigurationProvider;
+import com.cloudogu.scm.commitmessagechecker.config.Validation;
+import picocli.CommandLine;
+import sonia.scm.cli.CliContext;
+import sonia.scm.cli.ParentCommand;
+import sonia.scm.cli.TemplateRenderer;
+import sonia.scm.repository.NamespaceAndName;
+import sonia.scm.repository.Repository;
+import sonia.scm.repository.RepositoryManager;
+import sonia.scm.repository.cli.RepositoryCommand;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@ParentCommand(RepositoryCommand.class)
+@CommandLine.Command(name = "commit-message-check", aliases = "cmc", resourceBundle = "sonia.scm.cli.i18n")
+public class CommitMessageCheckerCommand implements Runnable {
+
+  @CommandLine.Parameters(paramLabel = "namespace/name", descriptionKey = "scm.repo.cmc.repository", index = "0")
+  private String repository;
+
+  @CommandLine.Parameters(paramLabel = "branch", descriptionKey = "scm.repo.cmc.branch", index = "1")
+  private String branch;
+
+  @CommandLine.Parameters(paramLabel = "commit-message", descriptionKey = "scm.repo.cmc.commitMessage", index = "2")
+  private String commitMessage;
+
+  @CommandLine.Mixin
+  private final TemplateRenderer templateRenderer;
+  private final CliContext context;
+
+  private final ConfigurationProvider configurationProvider;
+  private final AvailableValidators availableValidators;
+  private final RepositoryManager repositoryManager;
+
+  @Inject
+  public CommitMessageCheckerCommand(
+    TemplateRenderer templateRenderer,
+    CliContext context, ConfigurationProvider configurationProvider,
+    AvailableValidators availableValidators,
+    RepositoryManager repositoryManager
+  ) {
+    this.templateRenderer = templateRenderer;
+    this.context = context;
+    this.configurationProvider = configurationProvider;
+    this.availableValidators = availableValidators;
+    this.repositoryManager = repositoryManager;
+  }
+
+  @Override
+  public void run() {
+    String[] splitRepo = repository.split("/");
+    Repository repo = repositoryManager.get(new NamespaceAndName(splitRepo[0], splitRepo[1]));
+    configurationProvider.evaluateConfiguration(repo)
+      .ifPresent(
+        configuration -> {
+          try {
+            validate(configuration, repo);
+          } catch (RuntimeException e) {
+            templateRenderer.renderDefaultError(e);
+            context.exit(2);
+          }
+        });
+  }
+
+  private void validate(Configuration configuration, Repository repo) {
+    List<Validation> validations = configuration.getValidations();
+    for (Validation validation : validations) {
+      Validator validator = availableValidators.validatorFor(validation.getName());
+      validator.validate(new Context(repo, branch, validation.getConfiguration()), commitMessage);
+    }
+  }
+}
